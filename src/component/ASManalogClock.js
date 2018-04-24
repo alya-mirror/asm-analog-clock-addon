469652b89004ac7d22a01de23b7c89620ecdfcac
Unknown <alialsaeedi19@gmail.com>
2018-04-25 01:46:11 +0200

    fix errors

--- src/component/ASManalogClock.js
+++ src/component/ASManalogClock.js
@@ -1,12 +1,63 @@
 import React, {Component} from 'react'
-import AnalogClock, { Themes } from 'react-analog-clock';
-const width = 200;
+import AnalogClock, {Themes} from 'react-analog-clock';
+import io from 'socket.io-client';
 
 class ASManalogClock extends Component {
 
+  constructor(props) {
+    super(props);
+    let self = this;
+    this.state = {
+      settings: {
+        width: 100,
+        theme: Themes.lime
+      }
+    };
+    this.socket = io('http://localhost:3100');
+    this.socket.on('connect', function () {
+      console.log('connected');
+    });
+    this.socket.on('disconnect', function () {
+      console.log('disconnected');
+    });
+    this.socket.on('@alya-mirror/asm-analog-clock-addon', function (data) {
+      const message = JSON.parse(data).data.addonSettings;
+      let newTheme = Themes.black;
+      switch (message.theme){
+        case 'black':
+          newTheme = Themes.black;
+          break;
+        case 'light':
+          newTheme = Themes.light;
+          break;
+        case 'aqua':
+          newTheme = Themes.aqua;
+          break;
+        case 'lime':
+          newTheme = Themes.lime;
+          break;
+        case 'sherbert':
+          newTheme = Themes.sherbert;
+          break;
+        case 'navy':
+          newTheme = Themes.navy;
+          break;
+        default:
+          break;
+      }
+      self.setState({
+        settings: {
+          width: message.width,
+          theme : newTheme
+        }
+      });
+
+    });
+  }
+
   render() {
     return <div>
-      <AnalogClock theme={Themes.lime} width={width}/>
+      <AnalogClock theme={this.state.settings.theme} width={this.state.settings.width}/>
     </div>
   }
 }

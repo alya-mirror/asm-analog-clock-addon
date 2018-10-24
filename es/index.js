f5a88e3e59021d684891858fef20749ba89ba0fd
Unknown <alialsaeedi19@gmail.com>
2018-10-24 02:53:04 +0200

    adding unneeded files to git ignore

--- es/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import ASManalogClock from './component/ASManalogClock';
-
-render(React.createElement(ASManalogClock, null), document.getElementById('root'));
